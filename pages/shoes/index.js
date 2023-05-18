3471feb00f77c6b66e3780b650ab0c3fcc0298f7
birat051 <biratbhattacharjee_ma@srmunviv.edu.in>
2023-05-19 01:54:20 +0530

    displayed head and image component

---
+++ pages/shoes/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Head from 'next/head'
+import Image from 'next/image'
+import img from '../../public/shoe1.avif'
+
+function ShoesPage() {
+  return (
+    <div>
+        <Head>
+            <title>Shoes List</title>
+            <meta content='This is a page displaying different shoe products' name='description'></meta>
+        </Head>
+        <Image src={img} placeholder='blur' alt='red shoe by nike' height='500'/>
+        {['2','3','4','5'].map((path)=>{
+            return (
+            <div>
+            <Image key={path} src={`/shoe${path}.jpg`} height='500' width='300' blurDataURL='/blur.jpg'/>
+            </div>
+            )
+        })}
+    </div>
+  )
+}
+
+
+
+export default ShoesPage
